f45cbfd11a349e14c435460f593cbff17157bd5a
Stuart Schafer <stuartschafer@Stuarts-MacBook-Pro.local>
2017-04-19 16:12:26 -0400

    Initial files for the homework

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Ssf6XEVwbZcMhpbqr9xlfU8zA',
+  consumer_secret: 'J0ZNGMfqL9czGQ6cUDh2QOlFn3vLTFJYY9HLAFrrzvOrtQZ3Vx',
+  access_token_key: '854776232281600000-ayXgQ6YVncgRidK7y1OySAhIa5BOVFo',
+  access_token_secret: 'VvYIKf8u0VmW9GyqNt1OQwR31wZb5670FUzldu4xGetEa',
+}
